c939239a89c7869c2ea03aed04315a4977107636
MED49 <m.benradi@sdmbapolloteam.onmicrosoft.com>
2022-03-07 17:02:43 +0000

    addcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'products',pathMatch:'full'},
+  {path:'products',component:ProductsComponent},
+  {path:'cart',component:CardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
